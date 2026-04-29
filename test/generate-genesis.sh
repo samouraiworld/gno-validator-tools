7f312bfebbfb17af2e67cef7137524fda82b0dc1
louis <lours@samourai.coop>
2026-04-29 13:40:01 -0400

    Fix voting power configuration in generate-genesis.sh

--- test/generate-genesis.sh
+++ test/generate-genesis.sh
@@ -90,6 +90,16 @@ echo "🔓 Setting unrestricted address..."
 gnogenesis params set auth.unrestricted_addrs \
   "$UNRESTRICTED_ADDR" \
   -genesis-path "$GENESIS_FILE"
+echo "⚖️ Adjusting Voting Power..."
+
+# On utilise jq pour mettre à jour les puissances de vote
+# Ici on met 10 pour le 1 et le 2, et 9 pour le 3
+cat "$GENESIS_FILE" | jq '
+  .validators[0].power = "10" | 
+  .validators[1].power = "10" | 
+  .validators[2].power = "11"
+' > genesis.tmp && mv genesis.tmp "$GENESIS_FILE"
+
 
 echo "✅ Genesis successfully generated!"
 
